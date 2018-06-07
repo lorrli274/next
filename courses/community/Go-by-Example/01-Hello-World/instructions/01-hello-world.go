9f64becb59a3bacf0994033ac6342a0f1f4d0a1d
omartinez64 <39919540+omartinez64@users.noreply.github.com>
2018-06-07 17:27:49 -0400

    Update 01-hello-world.go

--- courses/community/Go-by-Example/01-Hello-World/instructions/01-hello-world.go
+++ courses/community/Go-by-Example/01-Hello-World/instructions/01-hello-world.go
@@ -1,7 +1,3 @@
-Go by Example: Hello World
-
-Our first program will print the classic “hello world” message. Here’s the full source code.
-
 
 package main
 
@@ -16,10 +12,4 @@ $ go build hello-world.go
 $ ls
 hello-world	hello-world.go
 $ ./hello-world
-hello world
 
-
-To run the program, put the code in hello-world.go and use go run.
-Sometimes we’ll want to build our programs into binaries. We can do this using go build.
-We can then execute the built binary directly.
-Now that we can run and build basic Go programs, let’s learn more about the language.
